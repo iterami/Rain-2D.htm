be6a712039afa29beb36fdd080f0bb2a903d6fff
Jan Horava <czechinthemail@gmail.com>
2013-05-15 20:45:52 -0700

    improved formatting

--- rain.js
+++ rain.js
@@ -2,23 +2,33 @@ function draw(){
     buffer.clearRect(0,0,width,height);
 
     /*add 2 randomly placed drops*/
-    i=1;
+    i = 1;
     do{
-        drops.push([random_number(width),0,2,7])
+        drops.push([
+            random_number(width),
+            0,
+            2,
+            7
+        ])
     }while(i--);
 
-    i=drops.length-1;
-    buffer.fillStyle='#aaf';
+    i = drops.length-1;
+    buffer.fillStyle = '#aaf';
     do{
         if(drops[i][1]>height){
             /*remove drop that reached bottom of screen*/
             drops.splice(i,1)
         }else{
             /*update drop position*/
-            drops[i][1]+=Math.random()*9+9;
+            drops[i][1] += Math.random()*9+9;
 
             /*draw drop*/
-            buffer.fillRect(drops[i][0],drops[i][1],drops[i][2],drops[i][3])
+            buffer.fillRect(
+                drops[i][0],
+                drops[i][1],
+                drops[i][2],
+                drops[i][3]
+            )
         }
     }while(i--)
 
@@ -32,17 +42,19 @@ function random_number(i){
     return Math.floor(Math.random()*i)
 }
 function resize(){
-    width=get('buffer').width=get('canvas').width=window.innerWidth;
-    height=get('buffer').height=get('canvas').height=window.innerHeight
+    width = get('buffer').width = get('canvas').width = window.innerWidth;
+    height = get('buffer').height = get('canvas').height = window.innerHeight
 }
-var buffer=canvas=height=i=width=0,
-drops=[];
 
-buffer=get('buffer').getContext('2d');
-canvas=get('canvas').getContext('2d');
+var buffer = get('buffer').getContext('2d');
+var canvas = get('canvas').getContext('2d');
+var drops = [];
+var height = 0;
+var i = 0;
+var width = 0;
 
 resize();
 
 setInterval('draw()',30);
 
-window.onresize=resize
+window.onresize = resize
