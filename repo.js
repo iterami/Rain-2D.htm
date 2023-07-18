262d816acdfed0e24ff0efecd1aa4c2f4846eb2a
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 14:26:04 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,18 @@
 'use strict';
 
+function load_data(){
+    entity_create({
+      'id': 'obstacle',
+      'properties': {
+        'x': canvas_properties['width-half'],
+        'y': canvas_properties['height-half'],
+      },
+      'types': [
+        'object',
+      ],
+    });
+}
+
 function repo_drawlogic(){
     canvas_setproperties({
       'properties': {
